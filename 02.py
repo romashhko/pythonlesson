5cb8113e4ac7c6b65846fe456782867a03efe094
romashhko <nastya.romashko2008@gmail.com>
2022-04-08 14:52:39 +0300

    last commit

--- 02.py
+++ 02.py
@@ -1,10 +1,36 @@
-def main(*args, **kwargs):
-  result = ''
-  
-  for number in args:
-     result += number
+class Cat:
+   def __init__(self):
+      self.year = 3
+      self.eats = 'many'
+      self.signal_in_6am = 'very very good'
+      self.motor_ = 'very good'
+      self.jump = 'good'
+      self.lazy = 'very'
 
-  print(args)
-  print(kwargs["staff"])
+   def eat(self):
+       print('їсть')
 
-main('п', 'р', 'и', 'в', 'і', 'т', name="Roma", age=21, staff="teacher" )
+   def sleep(self):
+       print('спить')
+
+   def play(self):
+       print('грає')
+
+   def signal(self):
+       print('кричить')
+   
+   def motor(self):
+       print('мурчить')
+
+   def toilet(self):
+       print('ходить в туалет')
+
+my_cat = Cat()
+
+my_cat.eat()
+my_cat.sleep()
+my_cat.play()
+my_cat.signal()
+my_cat.motor()
+my_cat.toilet()
+
